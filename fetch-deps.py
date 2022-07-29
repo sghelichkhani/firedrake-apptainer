694ff95f5d83e9799effc6f0a49114f365adcd1c
Angus Gibson <angus@agibson.me>
2022-07-29 19:36:07 +1000

    Modify fetch_deps for multiple packages

--- fetch-deps.py
+++ fetch-deps.py
@@ -64,13 +64,16 @@ def download_archives(archives):
     files_path = Path("files")
     files_path.mkdir(exist_ok=True)
 
-    with tqdm(archives.items()) as t:
-        for archive, url in t:
-            file_path = files_path / archive
+    with tqdm(archives.values()) as t:
+        for archive in t:
+            archive_path = archive["archive"]
+            url = archive["url"]
+
+            file_path = files_path / archive_path
             if file_path.exists():
                 continue
 
-            t.set_description(f"downloading {archive}")
+            t.set_description(f"downloading {archive_path}")
             r = requests.get(url, stream=True)
             with file_path.open("wb") as f:
                 for chunk in tqdm(r.iter_content(1024), unit="kB"):
@@ -83,7 +86,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description="Fetch Firedrake Apptainer dependencies",
-        epilog="With no arguments, performs all actions",
+        epilog="With no arguments, downloads git and archive dependencies for Firedrake",
     )
     parser.add_argument(
         "--repos", action="store_true", help="Clone or update all git repositories"
@@ -91,12 +94,64 @@ if __name__ == "__main__":
     parser.add_argument(
         "--archives", action="store_true", help="Download source code archives"
     )
+    parser.add_argument(
+        "--only",
+        help="Restrict downloaded dependencies to comma-separated list",
+    )
+    parser.add_argument(
+        "--prefer-archives",
+        action="store_true",
+        help="Prefer tarball archives to git repositories, for PETSc dependencies",
+    )
+    parser.add_argument(
+        "components",
+        nargs="*",
+        choices=[[], "firedrake", "petsc"],
+        help="Components of which to download dependencies",
+    )
     args = parser.parse_args()
 
-    do_all = not (args.repos or args.archives)
+    if args.only:
+        only_list = [dep.lower() for dep in args.only.split(",")]
+        config["repos"] = {
+            k: v for k, v in config["repos"].items() if k.lower() in only_list
+        }
+        config["archives"] = {
+            k: v for k, v in config["archives"].items() if k.lower() in only_list
+        }
+        config["petsc"] = {
+            k: v for k, v in config["petsc"].items() if k.lower() in only_list
+        }
 
-    if args.repos or do_all:
-        update_repos(config["repos"])
+    if "firedrake" in args.components or not args.components:
+        do_all = not (args.repos or args.archives)
+        if args.repos or do_all:
+            update_repos(config["repos"])
 
-    if args.archives or do_all:
-        download_archives(config["archives"])
+        if args.archives or do_all:
+            download_archives(config["archives"])
+
+    if "petsc" in args.components:
+        # whether we want to get things that are *only* available through git,
+        # or everything available through git
+        if args.prefer_archives:
+            repos = {
+                package: locations["git-url"]
+                for package, locations in config["petsc"].items()
+                if "archive" not in locations
+            }
+        else:
+            repos = {
+                package: locations["git-url"]
+                for package, locations in config["petsc"].items()
+                if "git-url" in locations
+            }
+
+        # download tarball archives for everything else
+        archives = {
+            package: locations
+            for package, locations in config["petsc"].items()
+            if package not in repos
+        }
+        update_repos(repos)
+        download_archives(archives)
